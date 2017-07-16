95e3b298a415bf71a17b1801d68bcdb994f8dbdc
bryankelly0077 <bryankelly0077@gmail.com>
2017-07-16 12:42:09 +0100

    All List Object Types

---
+++ KVDataBoundDDL/AllListControls.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AllListControls.aspx.cs" Inherits="KVDataBoundDDL.AllListControls" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 40px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            </asp:CheckBoxList>
+            <br />
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            </asp:RadioButtonList>
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server">
+            </asp:ListBox>
+            <br />
+            <asp:BulletedList ID="BulletedList1" runat="server">
+            </asp:BulletedList>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ KVDataBoundDDL/AllListControls.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KVDataBoundDDL
+{
+    public partial class AllListControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                AddListItems(DropDownList1);
+                AddListItems(CheckBoxList1);
+                AddListItems(RadioButtonList1);
+                AddListItems(ListBox1);
+                AddListItems(BulletedList1);
+            }
+        }
+        private void AddListItems(ListControl listControl)
+        {
+            ListItem li1 = new ListItem("Item1", "1");
+            ListItem li2 = new ListItem("Item2", "2");
+            ListItem li3 = new ListItem("Item3", "3");
+
+            listControl.Items.Add(li1);
+            listControl.Items.Add(li2);
+            listControl.Items.Add(li3);
+        }
+
+        private void RetrieveMultipleSelections(ListControl listControl)
+        {
+            foreach (ListItem li in listControl.Items)
+            {
+                if (li.Selected)
+                {
+                    Response.Write("Text = " + li.Text + ", Value = " + li.Value +
+                        ", Index = " + listControl.Items.IndexOf(li).ToString() + "<br/>");
+                }
+            }
+        }
+
+        private void RetrieveSelectedItemTextValueandIndex(ListControl listControl)
+        {
+            if (listControl.SelectedIndex != -1)
+            {
+                Response.Write("Text = " + listControl.SelectedItem.Text + "<br/>");
+                Response.Write("Value = " + listControl.SelectedItem.Value + "<br/>");
+                Response.Write("Index = " + listControl.SelectedIndex.ToString());
+            }
+        }
+
+        protected void BulletedList1_Click(object sender, BulletedListEventArgs e)
+        {
+            ListItem li = BulletedList1.Items[e.Index];
+            Response.Write("Text = " + li.Text + "<br/>");
+            Response.Write("Value = " + li.Value + "<br/>");
+            Response.Write("Index = " + e.Index.ToString());
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            RetrieveMultipleSelections(CheckBoxList1);
+            RetrieveSelectedItemTextValueandIndex(DropDownList1);
+            RetrieveSelectedItemTextValueandIndex(ListBox1);
+            RetrieveSelectedItemTextValueandIndex(BulletedList1);
+            RetrieveSelectedItemTextValueandIndex(RadioButtonList1);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            RetrieveSelectedItemTextValueandIndex(DropDownList1);
+            RetrieveSelectedItemTextValueandIndex(BulletedList1);
+            RetrieveSelectedItemTextValueandIndex(RadioButtonList1);
+        }
+    }
+}
